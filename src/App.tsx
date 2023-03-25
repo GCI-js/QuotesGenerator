71e2d38facde40d80cf1ff48ccb2921b81b5e22c
2sangjin <eesj96@gmail.com>
2023-03-25 21:57:05 +0900

    공유하기, 다운로드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { useState } from "react";
 
 import styles from "./App.module.scss";
 import MockTogonapshin from "./mock_data/mock_togonapshin.json";
-import StickerModal from "./component/StickerModal";
-import Title from "./component/title";
 import TitleHeader from "./component/TItleHeader";
+import desktop_img from "./desktop_img.svg";
+import share_img from "./share_img.svg";
 
 export default function App(properties: Properties) {
   const id = [`_${idiotproof.trace(Editor)}`, properties.id].join();
@@ -16,16 +16,42 @@ export default function App(properties: Properties) {
   const [togonapshinData, setTogonapshinData] = useState({
     ...MockTogonapshin[0],
   });
+  var isMobile = /Mobi/i.test(window.navigator.userAgent);
 
   function randomText() {
     let randomIndex = Math.floor(Math.random() * MockTogonapshin.length);
     setTogonapshinData(MockTogonapshin[randomIndex]);
   }
 
+  async function shareClicked() {
+    console.log("shareClicked....");
+    //navigator.share가 지원되는 경우
+    let data = {
+      title: "test",
+      //text: '',
+      url: "www.naver.com",
+    };
+    console.log("navigator....", navigator.canShare(data));
+
+    if (navigator.canShare(data)) {
+      await navigator.share(data);
+    }
+  }
+
   return (
     <div id={id} className={cl}>
       <TitleHeader />
+      {/* {!isMobile && (
+        <div className="forDesktop">
+          <img src={desktop_img}></img>
+        </div>
+      )} */}
       <Editor />
+      <img
+        className="Share"
+        src={share_img}
+        onClick={() => shareClicked()}
+      ></img>
       <TemporalBoard randomText={randomText} />
     </div>
   );
--- src/component/TemporalBoard/index.tsx
+++ src/component/TemporalBoard/index.tsx
@@ -11,6 +11,7 @@ import sticker_img from "./sticker_img.svg";
 import info_img from "./info_img.svg";
 import download_img from "./download_img.svg";
 import html2canvas from "html2canvas";
+import DownloadModal from "../DownloadModal";
 interface Props extends Properties {
   randomText: Function;
 }
@@ -18,36 +19,17 @@ export default function TemporalBoard(properties: Props) {
   const id = [`_${idiotproof.trace(TemporalBoard)}`, properties.id].join();
   const cl = [styles.index, properties.className].join(" ");
 
-  async function saveClicked() {
-    console.log("saveclicked....");
-    //navigator.share가 지원되는 경우
-    if (navigator.canShare) {
-      await navigator.share({
-        title: "test",
-        //text: '',
-        url: "www.naver.com",
-      });
-    } else {
-      html2canvas(document.getElementById("testCapture")).then((canvas) => {
-        onSaveAs(canvas.toDataURL("image/png"), "image_togonapshin.png");
-      });
-    }
-  }
-  function onSaveAs(uri: string, filename: string) {
-    var link = document.createElement("a");
-    document.body.appendChild(link);
-    link.href = uri;
-    link.download = filename;
-    link.click();
-    document.body.removeChild(link);
-  }
-
-  const [modalOpen, setModalOpen] = useState(false);
+  const [infoModalOpen, setinfoModalOpen] = useState(false);
+  const [downloadModalOpen, setdownloadModalOpen] = useState(false);
 
   // 모달창 노출
-  const showModal = () => {
-    console.log("clicked showModal");
-    setModalOpen(true);
+  const showInfoModal = () => {
+    console.log("clicked showInfoModal");
+    setinfoModalOpen(true);
+  };
+  const showdownloadModal = () => {
+    console.log("clicked showdownloadModal");
+    setdownloadModalOpen(true);
   };
 
   return (
@@ -83,10 +65,13 @@ export default function TemporalBoard(properties: Props) {
         </div>
       </div>
       <div className="InfoSave">
-        <img src={info_img} onClick={() => showModal()}></img>
-        <img src={download_img} onClick={() => saveClicked()}></img>
+        <img src={info_img} onClick={() => showInfoModal()}></img>
+        <img src={download_img} onClick={() => showdownloadModal()}></img>
       </div>
-      {modalOpen && <InfoModal setModalOpen={setModalOpen} />}
+      {infoModalOpen && <InfoModal setinfoModalOpen={setinfoModalOpen} />}
+      {downloadModalOpen && (
+        <DownloadModal setdownloadModalOpen={setdownloadModalOpen} />
+      )}
     </div>
   );
 }
